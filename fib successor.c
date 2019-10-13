43201f73205a0ca8f977604f7645ab30a84d16d9
Harshal Dupare <52428908+harshal3d@users.noreply.github.com>
2019-10-13 17:14:39 +0530

    Add files via upload

---
+++ fib successor.c
@@ -0,0 +1,27 @@
+#include<stdio.h>>
+
+int main(){
+
+int m,a,b,s=0,n,temp;
+
+printf("of which number do you want fib successor of\n");
+scanf("%d",&n);
+m=n;
+while(n>0){
+
+    a=1,b=1;
+
+    while(b<n){
+
+            temp=a;
+            a=b;
+            b=a+temp;
+
+    }
+
+    n=n-a,s=s+b;
+}
+
+printf("fibonacci successor of %d is %d",m,s);
+
+}
