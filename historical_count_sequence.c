e663fae27f4dbf653313786bf518fd6024cc16e6
Rohit-Yadav0 <ryaduvanshi253@gmail.com>
2019-10-15 22:16:04 +0530

    add

--- historical_count_sequence.c
+++ historical_count_sequence.c
@@ -7,15 +7,15 @@ int main()
 {
 
 int n,i,j,m,k;
-printf("give the size of starting sequence and the size of extended sequence \n");
+printf("give the size of starting sequence and the size of extended sequence:");
 scanf("%d %d",&n,&m);
 
 int a[m];
 
-printf("enter the sequence of %d numbers \n",n);
+printf("enter the sequence of %d numbers:",n);
 
-for(i=0;i<n;i++){
-    scanf("%d",&a[i]);
+for(j=0;j<n;j++){
+    scanf("%d",&a[j]);
 }
 
 for(i=0;i<m-n;i++){
